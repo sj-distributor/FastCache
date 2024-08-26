25a63431c43b6580c48773a3822627e13ed37d0d
https://gitlab.sjfood.us/Joe.G <joe.gao@sjfood.com>
2024-08-26 17:45:55 +0800

    增加分布式锁

---
+++ FastCache.Redis/Constant/Prefix.cs
@@ -0,0 +1,8 @@
+namespace FastCache.Redis.Constant
+{
+    public static class Prefix
+    {
+        public const string DeletePrefix = "lock:delete";
+        public const string AddPrefix = "lock:add";
+    }
+}
